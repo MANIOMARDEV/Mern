c80dd33c61f12b4e2b063b9d08a3efdaad1b65fb
MANIOMARDEV <144290707+MANIOMARDEV@users.noreply.github.com>
2024-02-13 22:35:12 +0100

    first commit

--- WEEK6/PRACTICE/refactor_reuse_recycle/server/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-module.exports = function(app){
-    app.get('/api', ProductController.index);
-    app.get('/api/product', ProductController.getAllProducts);
-    app.get('/api/product/:id', ProductController.getProductDetails);
-    app.post('/api/product', ProductController.createProduct);     
-    app.put('/api/product/:id', ProductController.updateProduct);
-    app.delete('/api/product/:id', ProductController.deleteProduct);
-}
